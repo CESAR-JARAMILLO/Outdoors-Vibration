c72c2bbe61e5143401caf889b7492efd9ebaa605
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-04-05 16:16:54 -0700

    Add Navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <b-navbar centered=true shadow=true>
+        <template #brand>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                Outdoor Vibrations
+            </b-navbar-item>
+        </template>
+        <template #start >
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                About
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Blog
+            </b-navbar-item>
+        </template>
+
+        <template #end>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Subscribe</strong>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+nav {
+    padding: 0 3.5rem;
+}
+</style>
