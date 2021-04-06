b8d42b83991c8853faa342d0feb0b56832f88754
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-04-06 11:23:52 -0700

    Add content

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,12 @@
 <template>
 <div>
   <Navbar/>
-  <img src="../assets/vector.png" alt="">
+  <h1 class="title">Connecting to Nature</h1>
+  <h2 class="subtitle">Have a love for all things that are outdoors?</h2>
+  <a href="">Join Now</a>
+  <a href="">Read Blog</a>
+  <img src="../assets/landing-pic.png" alt="">
+  <img class="vector" src="../assets/vector.png" alt="">
 </div>
 </template>
 
@@ -15,8 +20,22 @@ export default {
 </script>
 
 <style>
-img {
+.vector {
   position: absolute;
-  bottom: 0;
+  bottom: -5rem;
+  left: 0;
+  z-index: -1;
+}
+
+h1 {
+  margin-left: 3.5rem;
+}
+
+h2 {
+  margin-left: 3.5rem;
+}
+
+a {
+  margin-left: 3.5rem;
 }
 </style>
