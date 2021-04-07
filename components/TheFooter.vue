c661245e54e68986821f901a625dfa4b2f512ff4
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-04-07 09:10:56 -0700

    Create footer component

---
+++ components/TheFooter.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footer-container">
+      <div class="top-row">
+
+      </div>
+      <div class="bottom-row">
+
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'TheFooter'
+}
+</script>
+
+<style>
+.footer-container {
+    display: grid;
+    grid-template-rows: 2fr 1fr;
+    grid-template-areas: "top bottom"
+}
+
+.top-row {
+    grid-area: top;
+    background-color: #40916C;
+    height: 480px;
+}
+
+.bottom-row {
+    grid-area: bottom;
+    background-color: #1B4332;
+    height: 200px;
+}
+</style>
