e75d71dca11b2ebe57150acbb6b3273e87f4c5a6
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2021-05-27 09:18:12 -0700

    added Navbar

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-      
+      <Navbar />
   </div>
 </template>
 
 <script>
+import Navbar from '../components/Navbar'
 export default {
-
+    name: 'blogs',
+    components: {
+        Navbar
+    }
 }
 </script>
 
